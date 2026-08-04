1819ff76df127b7933f0c37a980f3ea04a909653
Kenji Noguchi <tokyo246@gmail.com>
2026-08-03 18:48:15 -0700

    Update marsdb to 0.5.0

--- Formula/marsdb.rb
+++ Formula/marsdb.rb
@@ -1,8 +1,8 @@
 class Marsdb < Formula
   desc "Embeddable property-graph database with an openCypher query subset"
   homepage "https://github.com/knoguchi/marsdb"
-  url "https://static.crates.io/crates/marsdb-cli/marsdb-cli-0.4.0.crate"
-  sha256 "c7055963c8da5d5ea52ab11e5a3cb7e79711d96e234e05d597c16cefd6b0d46b"
+  url "https://static.crates.io/crates/marsdb-cli/marsdb-cli-0.5.0.crate"
+  sha256 "49611bca5e1ba0254904bc739f1bbc1de6c47c04c615a43cd7b65db8506089fc"
   license any_of: ["Apache-2.0", "MIT"]
 
   depends_on "rust" => :build
